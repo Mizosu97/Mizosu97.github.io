96eb4a5cce2557117d9e9df578d8bb576ae217b0
Mizosu <97634559+Mizosu97@users.noreply.github.com>
2023-10-02 17:08:26 -0400

    Update repo.lua

--- packages/repo.lua
+++ packages/repo.lua
@@ -1,5 +1,6 @@
 packages = {
 	["official"] = {
+		["the-duat/mizos-supergfxctl"] = {"the-duat/mizos-supergfxctl", true},
 		["mizosu97/grapejuice"] = {"mizosu97/grapejuice", true},
 		["mizosu97/bastet"] = {"mizosu97/bastet", true}
 	},
